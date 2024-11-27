6b303d2d6798b57b26e607d420ed798637073bbb
sanwalsulehri <sanwalsulehri077@gmail.com>
2024-11-28 03:07:59 +0500

    done cards hover LINKS

--- components/BrowseCollection.vue
+++ components/BrowseCollection.vue
@@ -6,32 +6,52 @@
         collections</h1>
       <p class="text-[#000000] leading-relaxed font-light mt-6">Discover endless possibilities with our curated digital
         resources, perfect for all your creative needs.</p>
-      <div class="esm:grid hidden w-full mx-auto  sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10 mt-10">
-        <div v-for="(card, index) in cards" :key="index" class="hidden esm:flex group sm:w-auto flex-col gap-4">
-          <div class="group-hover: relative rounded-[10px] h-[220px] lg:h-[271px] w-full overflow-hidden">
-            <div class="w-full grid grid-cols-12 h-[291px] transition-all duration-500 group-hover:scale-[0.99] gap-[2px]">
-              <div class="col-span-8 transition-all duration-500  overflow-hidden h-full  w-full">
-                <img :src="card.image" class="h-full object-cover transition-all duration-500 w-full" alt="">
-              </div>
-              <div class="col-span-4 flex gap-[2px]  flex-col h-[220px] lg:h-full ">
-                <img v-for="(miniImage, idx) in card.miniImages" :key="idx" :src="miniImage"
-                  class="h-[50%] transition-all duration-500 object-cover w-full" alt="" />
-              </div>
+        <div class="esm:grid hidden w-full mx-auto sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10 mt-10">
+  <div v-for="(card, index) in cards" :key="index" class="hidden esm:flex sm:w-auto flex-col gap-4">
+    <div class="relative rounded-[10px] h-[220px] lg:h-[271px] w-full overflow-hidden">
+      <div class="w-full grid grid-cols-12 h-[291px] gap-[2px]">
+        <!-- Main Image -->
+        <a href="#1" class="col-span-8 relative group overflow-hidden h-full w-full">
+          <img :src="card.image" class="h-full object-cover group-hover:scale-[1.05]  transition-transform duration-500 w-full hover:scale-[1.05]" alt="">
+          <div class="w-full h-full transition-all duration-300 hover:bg-black/30 absolute top-0 left-0"></div>
+        </a>
 
-            </div>
+        <!-- Mini Images -->
+        <div class="col-span-4 relative overflow-hidden flex gap-[2px] flex-col h-[220px] lg:h-full">
+          <!-- First Mini Image -->
+          <a href="#2" class="relative h-1/2 overflow-hidden group">
+            <img v-for="(miniImage, idx) in card.miniImages.slice(0, 1)" :key="idx"
+              :src="miniImage"
+              class="h-[100%] transition-all duration-500 object-cover w-full group-hover:scale-[1.05] group-hover:opacity-80"
+              alt="" />
             <div class="w-full h-full transition-all duration-300 group-hover:bg-black/30 absolute top-0 left-0"></div>
-          </div>
-          <h1 class="lg:text-[20px] text-[18px] font-[700]">{{ card.title }}</h1>
-          <div class="flex flex-wrap w-full gap-2">
-            <button v-for="(btn, idx) in card.buttons" :key="idx"
-              class="bg-[#F5F5F5] hover:bg-[#DBF226] border border-[#EEEEEE] text-[#444444] rounded-[4px] sm:py-[5px] py-1 px-[7px] sm:px-[10px] lg:py-1.5 lg:px-3 text-[15px] font-light">
-              {{ btn }}
-            </button>
-          </div>
+          </a>
+
+          <!-- Second Mini Image -->
+          <a href="#3" class="relative h-1/2 overflow-hidden group">
+            <img v-for="(miniImage, idx) in card.miniImages.slice(1, 2)" :key="idx"
+              :src="miniImage"
+              class="h-[100%] transition-all duration-500 object-cover w-full group-hover:scale-[1.05] group-hover:opacity-80"
+              alt="" />
+            <div class="w-full h-full transition-all duration-300 group-hover:bg-black/30 absolute top-0 left-0"></div>
+          </a>
         </div>
-
-
       </div>
+    </div>
+
+    <!-- Title -->
+    <h1 class="lg:text-[20px] text-[18px] font-[700]">{{ card.title }}</h1>
+
+    <!-- Buttons -->
+    <div class="flex flex-wrap w-full gap-2">
+      <button v-for="(btn, idx) in card.buttons" :key="idx"
+        class="bg-[#F5F5F5] hover:bg-[#DBF226] border border-[#EEEEEE] text-[#444444] rounded-[4px] sm:py-[5px] py-1 px-[7px] sm:px-[10px] lg:py-1.5 lg:px-3 text-[15px] font-light">
+        {{ btn }}
+      </button>
+    </div>
+  </div>
+</div>
+
 
       <div
         class="gap-2 mt-0 w-full flex items-center justify-between absolute top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40 esm:hidden">
@@ -76,19 +96,36 @@
       }">
         <SplideSlide v-for="(card, index) in cards" :key="index"
           class=" flex flex-col   w-fit whitespace-nowrap overflow-auto   lg:grid-cols-3 gap-x-5 gap-y-6 mt-10">
-          <div class="group-hover: relative rounded-[10px] h-fit w-fit overflow-hidden">
-            <div class="w-full grid grid-cols-12 h-[291px] group-hover:scale-[1.05] transition-all duration-500 gap-[2px]">
-              <div class="col-span-8  overflow-hidden h-full  w-full">
-                <img :src="card.image" class="h-[100%]  transition-all duration-500 w-full" alt="">
-              </div>
-              <div class="col-span-4 flex flex-col h-[291px] mt-[1px] overflow-hidden">
-                <img v-for="(miniImage, idx) in card.miniImages" :key="idx" :src="miniImage"
-                  class="h-[50%] transition-all duration-500 object-cover w-full" alt="" />
-              </div>
+          <div class="relative rounded-[10px] h-[220px] lg:h-[271px] w-full overflow-hidden">
+      <div class="w-full grid grid-cols-12 h-[291px] gap-[2px]">
+        <!-- Main Image -->
+        <a href="#1" class="col-span-8 relative group overflow-hidden h-full w-full">
+          <img :src="card.image" class="h-full object-cover group-hover:scale-[1.05]  transition-transform duration-500 w-full hover:scale-[1.05]" alt="">
+          <div class="w-full h-full transition-all duration-300 hover:bg-black/30 absolute top-0 left-0"></div>
+        </a>
 
-            </div>
+        <!-- Mini Images -->
+        <div class="col-span-4 relative overflow-hidden flex gap-[2px] flex-col h-[220px] lg:h-full">
+          <!-- First Mini Image -->
+          <a href="#2" class="relative h-1/2 overflow-hidden group">
+            <img v-for="(miniImage, idx) in card.miniImages.slice(0, 1)" :key="idx"
+              :src="miniImage"
+              class="h-[100%] transition-all duration-500 object-cover w-full group-hover:scale-[1.05] group-hover:opacity-80"
+              alt="" />
             <div class="w-full h-full transition-all duration-300 group-hover:bg-black/30 absolute top-0 left-0"></div>
-          </div>
+          </a>
+
+          <!-- Second Mini Image -->
+          <a href="#3" class="relative h-1/2 overflow-hidden group">
+            <img v-for="(miniImage, idx) in card.miniImages.slice(1, 2)" :key="idx"
+              :src="miniImage"
+              class="h-[100%] transition-all duration-500 object-cover w-full group-hover:scale-[1.05] group-hover:opacity-80"
+              alt="" />
+            <div class="w-full h-full transition-all duration-300 group-hover:bg-black/30 absolute top-0 left-0"></div>
+          </a>
+        </div>
+      </div>
+    </div>
           <h1 class="lg:text-[20px]   font-bold text-[18px]">{{ card.title }}</h1>
           <div class="flex items-start justify-start flex-wrap min-h-20 gap-y-2   w-full gap-2">
             <button v-for="(btn, idx) in card.buttons" :key="idx"
